32fe4b686e29c392ef7a2f66b1b9c182bd6863bc
lavrentious <lavrent0@yandex.ru>
2025-05-07 13:03:10 +0300

    style(client): display result info

--- client/src/modules/approximation/components/ApproximationVisualization.tsx
+++ client/src/modules/approximation/components/ApproximationVisualization.tsx
@@ -1,4 +1,5 @@
 import React, { useMemo } from "react";
+import { Badge, Table } from "react-bootstrap";
 import Plot from "react-plotly.js";
 import { ApproximationResponse } from "../api/types";
 import { fExprToFunction, generatePoints, pointsToXsYs } from "../utils";
@@ -26,7 +27,6 @@ const ApproximationVisualization: React.FC<ApproximationVisualizationProps> = ({
 
   return (
     <>
-      results: todo
       <Plot
         data={[
           {
@@ -52,6 +52,52 @@ const ApproximationVisualization: React.FC<ApproximationVisualizationProps> = ({
           yaxis: { title: { text: "ф(x)" } },
         }}
       />
+      <Table bordered striped>
+        <tbody>
+          <tr>
+            <td>status</td>
+            <td>
+              {result.success ? (
+                <Badge bg="success">success</Badge>
+              ) : (
+                <Badge bg="danger">failed</Badge>
+              )}
+            </td>
+          </tr>
+          <tr>
+            <td>used method</td>
+            <td>{result.method}</td>
+          </tr>
+          {result.data && (
+            <>
+              <tr>
+                <td>params</td>
+                <td>
+                  {Object.entries(result.data.parameters).map(([k, v]) => (
+                    <div key={k}>
+                      {k}: {v}
+                    </div>
+                  ))}
+                </td>
+              </tr>
+              <tr>
+                <td>function</td>
+                <td>{result.data.f_expr}</td>
+              </tr>
+            </>
+          )}
+          <tr>
+            <td>message</td>
+            <td>
+              {result.message ? (
+                result.message
+              ) : (
+                <span className="text-muted fst-italic">n/a</span>
+              )}
+            </td>
+          </tr>
+        </tbody>
+      </Table>
     </>
   );
 };
